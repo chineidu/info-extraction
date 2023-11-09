ec0660802d84ee6864859d1bfb6f61f34a2215d1
Chinedu Ezeofor <Chineiduezeofor@gmail.com>
2023-11-09 18:38:56 +0100

    chore: Add module for making predictions

--- src/info_extraction/predict.py
+++ src/info_extraction/predict.py
@@ -1,4 +1,4 @@
-from typing import Any, TypeVar, Union
+from typing import Any, TypeAlias, Union
 
 from transformers import pipeline
 from typeguard import typechecked
@@ -11,8 +11,8 @@ logger = get_console_logger()
 AGGREGATION_STRATEGY: str = config.model_config_schema.AGGREGATION_STRATEGY
 TASK: str = config.model_config_schema.TASK
 TRAINED_MODEL_CHECKPOINT: str = config.model_config_schema.TRAINED_MODEL_CHECKPOINT
-ModelInput: TypeVar = Union[str, list[str]]  # type: ignore[assignment]
-Predictions: TypeVar = Union[list[dict[str, Any]], list[list[dict[str, Any]]]]  # type: ignore[assignment]
+ModelInput: TypeAlias = Union[str, list[str]]
+Predictions: TypeAlias = Union[list[dict[str, Any]], list[list[dict[str, Any]]]]
 
 
 @typechecked
@@ -28,11 +28,11 @@ def _load_model() -> Any:
 
 
 @typechecked
-def predict(model_input: ModelInput) -> Predictions:  # type: ignore[valid-type]
+def predict(model_input: ModelInput) -> Predictions:
     """This is used to make predictions using the NER language model."""
     token_classifier: pipeline = _load_model()
-    logger.info("Making prediction ...")
-    result: Predictions = token_classifier(model_input)  # type: ignore[valid-type]
+    logger.info(" >>>> Making prediction <<<<")
+    result: Predictions = token_classifier(model_input)
     logger.info("Prediction successfully made!")
     return result
 
@@ -44,10 +44,14 @@ if __name__ == "__main__":
         "Mauricio Pochettino is the head coach of Chelsea FC.",
         "Olumo Rock is a landmark in Abeokuta, Nigeria.",
         "Lionel Messi won the latest edition of the FIFA World Cup in 2022.",
-        """Solvify is a technology company that specializes in Internet-related
-        services and products.""",
+        (
+            "Solvify is a technology company that specializes in Internet-related"
+            + "services and products."
+        ),
         "The Harry Potter series, authored by J.K. Rowling, remains a bestseller worldwide.",
-        """On September 20, 2023, Apple unveiled its latest iPhone model at the tech conference
-        in San Francisco.""",
+        (
+            "On September 20, 2023, Apple unveiled its latest iPhone model at the tech"
+            + "conference in San Francisco."
+        ),
     ]
     print(predict(texts))
